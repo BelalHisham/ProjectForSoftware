6613f372d84ae9cf8c9bc178817aea64d2604216
Belal Hisham <bh2062001.bh@gmail.com>
2022-05-18 18:15:23 +0200

    Stones to the boxes

--- src/main/java/GameController.java
+++ src/main/java/GameController.java
@@ -1,16 +1,19 @@
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
 import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
-
 import org.tinylog.Logger;
 
 public class GameController {
 
     @FXML
     private GridPane board;
+
 
     @FXML
     private void initialize() {
@@ -24,9 +27,15 @@ public class GameController {
 
 
 
+
+
     private StackPane createBox() {
         var box = new StackPane();
-        box.getStyleClass().add("square");
+        box.getStyleClass().add("box");
+        var stone = new Circle(30);
+        stone.setFill(Color.TRANSPARENT);
+        box.getChildren().add(stone);
+       // box.getChildren().remove(stone);
         box.setOnMouseClicked(this::handleMouseClick);
         return box;
     }
@@ -36,11 +45,22 @@ public class GameController {
         var box = (StackPane) event.getSource();
         var row = GridPane.getRowIndex(box);
         var col = GridPane.getColumnIndex(box);
+        var stone = (Circle) box.getChildren().get(0);
+        stone.setFill(takeAwayAStone((Color) stone.getFill()));
 
         // find a solution for this sout or delete it
         System.out.printf("Click on box (%d,%d)%n", row, col);
        // Logger.debug("Click on box (%d,%d)", row, col);
         clickedOnBox();
+
+
+
+    }
+    private Color takeAwayAStone(Color color) {
+        if (color == Color.TRANSPARENT) {
+            return Color.SLATEGRAY;
+        }
+        return Color.SLATEGRAY;
 
     }
 
@@ -48,5 +68,7 @@ public class GameController {
     private void clickedOnBox(){
 
         Logger.info("You clicked in a box ");
+
+
     }
 }
